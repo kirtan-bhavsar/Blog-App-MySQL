215e48e82afff6887c52311875922d624bda646d
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-07-09 10:43:25 +0530

    successfullyImplementedRichTextEditor_Write.jsx_Dt:09072025

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useState, useRef, useMemo } from 'react';
+import JoditEditor from 'jodit-react';
+
 
 const Write = () => {
+
+  const editor = useRef(null);
+  const [content, setContent] = useState('');
+  console.log(content);
+
   return (
     <div className='add'>
-      <div className="content">content</div>
+      <div className="content">
+        <input type="text" placeholder='label' />
+        <div className="editorContainer">
+          <JoditEditor
+            ref={editor}
+            value={content}
+            onChange={newContent => { setContent(newContent) }}
+            autofocus
+          />
+        </div>
+      </div>
       <div className="menu">
         <div className="item">i1</div>
         <div className="item">i2</div>
@@ -11,5 +29,6 @@ const Write = () => {
     </div>
   )
 }
+
 
 export default Write
